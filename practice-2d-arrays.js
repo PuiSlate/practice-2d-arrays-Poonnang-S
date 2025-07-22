e8b150e71eb3e5ad649d5a21e45cb06727a11eff
PuiSlate <pkaraket73@gmail.com>
2025-07-21 22:00:46 -0500

    Add files via upload

---
+++ practice-2d-arrays.js
@@ -0,0 +1,19 @@
+//Challenge 1. 
+let allScores = [
+    [65, 79, 88],
+    [84, 69, 75],
+    [77, 95, 54]
+];
+
+//Challenge 2. 
+let theaterSeating = [
+    ["A1", "A2", "A3", "A4"],
+    ["B1", "B2", "B3", "B4"],
+    ["C1", "C2", "C3", "C4"]
+];
+
+//Challenge 3.
+let contactsList = [
+    ["Alice", "alice@gmail.com", "3142563254"],
+    ["Michael", "michael@gmail.com", "5731248934"],
+];
